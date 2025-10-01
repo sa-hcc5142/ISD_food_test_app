85ec411a7fce322a7c0b3d8e2effdd9a5a08dfa7
adnan <adnan.siraz09@gmail.com>
2025-10-01 16:33:40 +0600

    Authenticate + Realtime DB + google-services.json Done successfully

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,19 +33,36 @@ android {
         dataBinding = true;
     }
 }
-
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-//    implementation ("com.google.android.material:material:1.12.0")
+    implementation(libs.firebase.auth)
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
+    implementation(libs.firebase.database)
+
     implementation ("com.google.code.gson:gson:2.11.0")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation ("com.google.firebase:firebase-auth:22.3.0")
-    implementation(libs.firebase.database)
+
+    // force API-34-compatible versions
+    implementation(libs.core)
+    implementation(libs.core.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
+
+configurations.all {
+    resolutionStrategy {
+        force("androidx.credentials:credentials:1.3.0")
+        force("androidx.credentials:credentials-play-services-auth:1.3.0")
+        force("androidx.core:core:1.12.0")
+        force("androidx.core:core-ktx:1.12.0")
+    }
+}
+
---
+++ app/src/main/java/com/example/food_app_test/Activity/BaseActivity.java
@@ -0,0 +1,27 @@
+package com.example.food_app_test.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.food_app_test.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class BaseActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    public String TAG = "uilover";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+    }
+}
