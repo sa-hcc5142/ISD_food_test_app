da4df1a5333996d79a786d627d91f0efca6081b8
adnan <adnan.siraz09@gmail.com>
2025-10-11 15:18:55 +0600

    Changed the getValue() method

---
+++ app/src/main/java/com/example/food_app_test/data/entities/PriceEntity.java
@@ -0,0 +1,25 @@
+package com.example.food_app_test.data.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "prices")
+public class PriceEntity {
+    @PrimaryKey
+    private int id;
+    private String value;
+
+    public PriceEntity() {}
+
+    public PriceEntity(int id, String value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    // Getters and Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getValue() { return value.toUpperCase(); }
+    public void setValue(String value) { this.value = value; }
+}
